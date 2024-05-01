20e11978cd786979fb1e0ad24144511fb8eac1c7
Letícia Frasquette <frasquetteleticia@gmail.com>
2024-05-01 15:51:38 -0300

    Alteração na utilização do useSearchParams

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,19 +1,15 @@
 'use client'
 
 import { Skeleton } from '@/components/skeleton'
-import { useSearchParams } from 'next/navigation'
 import { Suspense } from 'react'
+import { CurrentSearch } from './current-search'
 
-function Loading() {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
-
+export default function SearchLoading() {
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Results for: <span className="font-semibold">{query}</span>
-      </p>
+      <Suspense fallback={null}>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[400px]" />
@@ -24,13 +20,5 @@ function Loading() {
         <Skeleton className="h-[400px]" />
       </div>
     </div>
-  )
-}
-
-export default function SearchLoading() {
-  return (
-    <Suspense>
-      <Loading />
-    </Suspense>
   )
 }
